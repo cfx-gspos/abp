83b055e8a2a1688ac28a39c1d43aeb9c6f39c1d2
frank@softwaretailor.com <frank@softwaretailor.com>
2020-04-02 16:37:47 +0800

    init.

---
+++ src/Acme.BookStore.EntityFrameworkCore/EntityFrameworkCore/BookStoreDbContextModelCreatingExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Volo.Abp;
+using Volo.Abp.Users;
+using Volo.Abp.EntityFrameworkCore.Modeling;
+
+namespace Acme.BookStore.EntityFrameworkCore
+{
+    public static class BookStoreDbContextModelCreatingExtensions
+    {
+        public static void ConfigureBookStore(this ModelBuilder builder)
+        {
+            Check.NotNull(builder, nameof(builder));
+
+            /* Configure your own tables/entities inside here */
+
+            //builder.Entity<YourEntity>(b =>
+            //{
+            //    b.ToTable(BookStoreConsts.DbTablePrefix + "YourEntities", BookStoreConsts.DbSchema);
+
+            //    //...
+            //});
+            builder.Entity<Book>(b =>
+            {
+                b.ToTable(BookStoreConsts.DbTablePrefix + "Books", BookStoreConsts.DbSchema);
+                b.ConfigureByConvention(); //auto configure for the base class props
+                b.Property(x => x.Name).IsRequired().HasMaxLength(128);
+            });
+        }
+
+        public static void ConfigureCustomUserProperties<TUser>(this EntityTypeBuilder<TUser> b)
+            where TUser: class, IUser
+        {
+            //b.Property<string>(nameof(AppUser.MyProperty))...
+        }
+    }
+}
---
+++ src/Acme.BookStore.Web/BookStoreWebAutoMapperProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace Acme.BookStore.Web
+{
+    public class BookStoreWebAutoMapperProfile : Profile
+    {
+        public BookStoreWebAutoMapperProfile()
+        {
+            //Define your AutoMapper configuration here for the Web project.
+            CreateMap<BookDto, CreateUpdateBookDto>();
+        }
+    }
+}
